1da29ab8b791c7f8766c0e66e5747648c2c9f6f4
KevinSilvester <kevinsilvester05@gmail.com>
2022-12-11 11:05:24 +0000

    feat: added arduino millis, removed unused specs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,44 @@
 #![no_std]
 #![no_main]
+#![feature(abi_avr_interrupt)]
+#![feature(const_option)]
 
+// use arduino_hal::{default_serial, delay_ms, pins, prelude::*, Adc, DefaultClock, Peripherals};
+// use panic_halt as _;
+// use ufmt::uwriteln;
+
+// mod time;
 use arduino_hal as hal;
 use arduino_hal::prelude::*;
 use panic_halt as _;
 use ufmt::uwriteln;
 
-/// TODO: Fix Heart Rate Reading (not accurate)
-
-#[arduino_hal::entry]
+#[no_mangle]
 fn main() -> ! {
+    // let dp = Peripherals::take().unwrap();
+    // let pins = pins!(dp);
+    // let mut adc = Adc::new(dp.ADC, Default::default());
+
+    // let mut serial = default_serial!(dp, pins, 115200);
+    // let a0 = pins.a0.into_analog_input(&mut adc);
+    // let a1 = pins.a1.into_analog_input(&mut adc);
+
+    // // let clock = time::TimerClock::<DefaultClock>::new(dp.TC0, time::Resolution::_1_MS).unwrap();
+    // // unsafe { avr_device::interrupt::enable() };
+
+    // loop {
+    //     let sweat_rate = a0.analog_read(&mut adc);
+    //     // let sweat_rate = 0;
+    //     let heart_rate = a1.analog_read(&mut adc);
+    //     // let timestamp = clock.millis();
+
+    //     // uwriteln!(&mut serial, "{},{},{}", sweat_rate, heart_rate, timestamp);
+    //     uwriteln!(&mut serial, "{},{}", sweat_rate, heart_rate);
+    //     delay_ms(100);
+    // }
     let dp = hal::Peripherals::take().unwrap();
     let pins = hal::pins!(dp);
-    let mut serial = hal::default_serial!(dp, pins, 57600);
+    let mut serial = hal::default_serial!(dp, pins, 115200);
     let mut adc = hal::Adc::new(dp.ADC, Default::default());
 
     let a0 = pins.a0.into_analog_input(&mut adc);
